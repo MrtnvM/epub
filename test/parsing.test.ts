fcd4a7f479cfa8086edea310f6fd3eb835500e9c
Maxim Martynov <max.mrtnv@gmail.com>
2021-08-21 16:17:22 +0400

    Parsting test updates

--- test/parsing.test.ts
+++ test/parsing.test.ts
@@ -18,17 +18,33 @@ const parseBook = async (path: string) => {
 
 mocha.describe("EPub books parsing", () => {
   mocha.it("Check parsing metadata", async () => {
-    const epubBooksPaths = fs.readdirSync("./example/epub_books");
+    const epubBooksPaths = fs
+      .readdirSync("./example/epub_books")
+      .filter((p) => p.endsWith(".epub"));
 
     for (const book of epubBooksPaths) {
       const bookPath = getBookPath(book);
-      const epub = await parseBook(bookPath);
+      let epub;
+      let isBookParsed = false;
 
-      assert.ok(epub.metadata.title);
-      assert.ok(epub.metadata.description);
-      assert.ok(epub.metadata.cover);
-      assert.ok(epub.metadata.date);
-      assert.ok(epub.metadata.creator);
+      try {
+        epub = await parseBook(bookPath);
+        isBookParsed = true;
+
+        assert.ok(epub.metadata.title);
+        assert.ok(epub.metadata.description);
+        assert.ok(epub.metadata.cover);
+        assert.ok(epub.metadata.date);
+        assert.ok(epub.metadata.creator);
+      } catch (error) {
+        console.error("FAILED TO PARSE BOOK: " + book);
+
+        if (isBookParsed) {
+          console.log(epub.metadata);
+        }
+
+        throw error;
+      }
     }
   });
 });
